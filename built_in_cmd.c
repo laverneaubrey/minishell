54d3d76e7067f1453dea1b4d122fa9b20bce4ccb
laverneaubrey <92808223+laverneaubrey@users.noreply.github.com>
2021-12-24 17:45:05 +0300

    Add files via upload

--- built_in_cmd.c
+++ built_in_cmd.c
@@ -8,15 +8,15 @@ int	ft_built_in_cmd(char **argv, char **env)
 //		ft_pwd(env);
 	else if (!ft_strncmp(argv[0], "env\0", 4))
 		ft_env(argv, env);
-//	if (!ft_strncmp(argv[0], "export\0", 7))
-//		ft_export(argv, env);
-//	if (!ft_strncmp(argv[0], "unset\0", 6))
-//		ft_unset(argv, env);
-//	if (!ft_strncmp(argv[0], "cd\0", 3))
-//		ft_cd(argv, env);
-//	if (!ft_strncmp(argv[0], "exit\0", 5))
-//		ft_exit(argv, env);
-//	else
-//		return (0);
+	if (!ft_strncmp(argv[0], "export\0", 7))
+		ft_export(argv, env);
+	if (!ft_strncmp(argv[0], "unset\0", 6))
+		ft_unset(argv, env);
+	if (!ft_strncmp(argv[0], "cd\0", 3))
+		ft_cd(argv, env);
+	if (!ft_strncmp(argv[0], "exit\0", 5))
+		ft_exit(argv, env);
+	else
+		return (0);
 	return (0);
 }
